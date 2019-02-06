92c2140e32d50b27dbc94319768020424bd340b6
jerlogan <jeremy.logan.web@gmail.com>
2019-02-06 16:35:58 -0600

    added new JS exercises

---
+++ js/conditionals.js
@@ -0,0 +1,218 @@
+"use strict";
+
+/**
+ * TODO:
+ * Write some JavaScript that uses a `confirm` dialog to ask the user if they
+ * would like to enter a number. If they click 'Ok', prompt the user for a
+ * number, then use 3 separate alerts to tell the user:
+ *
+ * - whether the number is even or odd
+ * - what the number plus 100 is
+ * - if the number is negative or positive
+ *
+ * if what the user enters is not a number, use an alert to tell them that, and
+ * do *not* display any of the above information.
+ *
+ * Can you refactor your code to use functions?
+ */
+
+Even or odd
+
+confirm("Would you like to enter a number?");
+var pickedNumber = prompt('What is your number?');
+
+if (pickedNumber % 2 === 0) {
+    alert('Your number is even.')
+}
+else {
+    alert('Your number is odd.')
+}
+
+//number plus 100
+
+alert('Your number plus 100 is ' + (parseInt(pickedNumber) + 100) + '.');
+
+//if number is negative or positive
+
+if (pickedNumber < 0) {
+    alert('Your number is negative.');
+}
+
+else {
+    alert('Your number is positive.');
+}
+
+/* ########################################################################## */
+
+/**
+ * TODO:
+ * Create a function named `analyzeColor` that accepts a string that is a color
+ * name as input. This function should return a message that related to that
+ * color. Only worry about the colors defined above, if the color passed is not
+ * one of the ones defined above, return a message that says so
+ *
+ * Example:
+ *  > analyzeColor('blue') // returns "blue is the color of the sky"
+ *  > analyzeColor('red') // returns "Strawberries are red"
+ *  > analyzeColor('cyan') // returns "I don't know anything about cyan"
+ *
+ * You should use an if-else-if-else block to return different messages.
+ *
+ * Test your function by passing various string literals to it and
+ * console.logging the function's return value
+ */
+
+//The following code is commented out because switch case statement below does the same thing (see line 86).
+
+// function analyzeColor(color) {
+//
+//     if (color === 'blue') {
+//         return ('blue is the color of the sky!');
+//     }
+//
+//     else if (color === 'red') {
+//         return ('Strawberries are red');
+//     }
+//
+//     else if (color === 'cyan') {
+//         return ("I don't know anything about cyan.");
+//     }
+//
+//     else {
+//         return ("Sorry, I don't know that color yet.");
+//     }
+// }
+
+function analyzeColor(color) {
+
+    switch (color) {
+        case 'blue':
+            return ('blue is the color of the sky!');
+            break;
+        case 'red':
+            return ('Strawberries are red');
+            break;
+        case 'cyan':
+            return ("I don't know anything about cyan.");
+            break;
+        default:
+            return ("Sorry, I don't know that color yet.");
+            break;
+    }
+}
+
+
+
+console.log(analyzeColor('blue'));
+console.log(analyzeColor('red'));
+console.log(analyzeColor('cyan'));
+console.log(analyzeColor('green'));
+
+
+
+// Don't change the next two lines!
+// These lines create two variables for you:
+// - `colors`: a list of the colors of the rainbow
+// - `randomColor`: contains a single random color value from the list (this
+//                  will contain a different color everytime the page loads)
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var randomColor = colors[Math.floor(Math.random() * colors.length)];
+/**
+ * TODO:
+ * Pass the `randomColor` variable to your function and console.log the results.
+ * You should see a different message everytime you refresh the page
+ */
+
+console.log(analyzeColor(randomColor));
+
+/**
+ * TODO:
+ * Refactor your above function to use a switch-case statement
+ */
+
+//see line 86
+
+
+/**
+ * TODO:
+ * Prompt the user for a color when the page loads, and pass the input from the
+ * user to your `analyzeColor` function. Alert the return value from your
+ * function to show it to the user.
+ */
+
+var userColor = analyzeColor(prompt('type a color.').toLowerCase());
+alert(userColor);
+
+
+/* ########################################################################## */
+
+/**
+ * TODO:
+ * Suppose there's a promotion in Walmart, each customer is given a randomly
+ * generated "lucky number" between 0 and 5. If your lucky number is 0 you have
+ * no discount, if your lucky number is 1 you'll get a 10% discount, if it's 2,
+ * the discount is 25%, if it's 3, 35%, if it's 4, 50%, and if it's 5 you'll get
+ * all for free!.
+ *
+ * Write a function named `calculateTotal` that accepts a lucky number and total
+ * amount, and returns the discounted price.
+ *
+ * Example:
+ * calculateTotal(0, 100) // returns 100
+ * calculateTotal(4, 100) // returns 50
+ * calculateTotal(5, 100) // returns 0
+ *
+ * Test your function by passing it various values and checking for the expected
+ * return value.
+ */
+
+
+function calculateTotal(luckyNumber, totalAmount) {
+    switch (luckyNumber) {
+        case 1:
+            return totalAmount - (totalAmount * .1);
+            break;
+        case 2:
+            return totalAmount - (totalAmount * .25);
+            break;
+        case 3:
+            return totalAmount - (totalAmount * .35);
+            break;
+        case 4:
+            return totalAmount - (totalAmount * .5);
+            break;
+        case 5:
+            return 0;
+            break;
+        default:
+            return totalAmount;
+            break;
+    }
+}
+
+    console.log(calculateTotal(0, 100));
+    console.log(calculateTotal(1, 100));
+    console.log(calculateTotal(2, 100));
+    console.log(calculateTotal(3, 100));
+    console.log(calculateTotal(4, 100));
+    console.log(calculateTotal(5, 100));
+
+/**
+ * TODO:
+ * Uncomment the line below to generate a random number between 0 and 6.
+ * Prompt the user for their total bill, then use your `calculateTotal` function
+ * and alerts to display to the user what their lucky number was, what their
+ * price before the discount was, and what their price after the discount is.
+ */
+// Generate a random number between 0 and 6
+var luckyNumber = Math.floor(Math.random() * 6);
+
+var userTotalBill = prompt('What is your total bill?');
+
+
+var google = parseInt((prompt('how much do you make?')).replace('$', ''));
+alert(google);
+
+
+
+
---
+++ js/external.js
@@ -0,0 +1,89 @@
+// console.log('Hello from external JS!');
+//
+// alert('Welcome to my website!');
+// var userFavColor = prompt('What is your favorite color?');
+// alert('Great, ' + userFavColor + ' is my favorite color too!');
+//
+// //movie prices logic
+//
+// var pricePernight = prompt('How much is each movie per night?');
+// var littleMermaidNights = prompt('For how many nights are you renting The Little Mermaid?')
+// var brotherBearNights = prompt('For how many nights are you renting Brother Bear?')
+// var herculesNights = prompt('For how many nights are you renting Hercules?')
+// var littleMermaid = pricePernight * littleMermaidNights;
+// var brotherBear = pricePernight * brotherBearNights;
+// var hercules = pricePernight * herculesNights;
+//
+// var total = littleMermaid + brotherBear + hercules;
+//
+// alert ('Your total price for renting all movies is $' + total + '.');
+//
+// // company payment logic
+//
+// var googleRate = parseInt((prompt('How much does Google pay per hour?')).replace('$', ''));
+// var amazonRate = parseInt((prompt('How much does Amazon pay per hour?')).replace('$', ''));
+// var facebookRate = parseInt((prompt('How much does Facebook pay per hour?')).replace('$', ''));
+// var googleWorkedHours = prompt('How many hours did you work at Google this week?');
+// var amazonWorkedHours = prompt('How many hours did you work at Amazon this week?');
+// var facebookWorkedHours = prompt('How many hours did you work at Facebook this week?');
+// var googleEarned = googleRate * googleWorkedHours;
+// var amazonEarned = amazonRate * amazonWorkedHours;
+// var facebookEarned = facebookRate * facebookWorkedHours;
+// var total = googleEarned + amazonEarned + facebookEarned;
+//
+// alert('You worked at Google for ' + googleWorkedHours + ' hours, paying $' + googleRate + ' per hour. You earned $' + googleEarned + ' with Google.' +
+//            'You worked at Amazon for ' + amazonWorkedHours + ' hours, paying $' + amazonRate + ' per hour. You earned $' + amazonEarned + ' with Amazon.' +
+//            'You worked at Facebook for ' + facebookWorkedHours + ' hours, paying $' + facebookRate + ' per hour. You earned $' + facebookEarned + ' with Facebook.' +
+//            'After all this hard work, you have earned a toal of $' + total + '. Great job!');
+
+//student enrollment logic
+
+var isClassFull = prompt('Is the class full?');
+var doesScheduleConflict = prompt('Does this class conflict with your schedule?');
+
+if (isClassFull.toLowerCase() === 'no' && doesScheduleConflict.toLowerCase() === 'no')  {
+    alert('Congrats! You can take the class.');
+}
+
+else {
+    alert('Sorry, you cannot take the class at this time.');
+}
+
+
+//buying product logic
+
+var productsBought = prompt('how many items do you have?');
+var expired = prompt('Has the offer expired?');
+var premiumMember = prompt ('Are you a premium member?');
+
+if (expired.toLowerCase() === 'yes') {
+    alert('Sorry, your offer is invalid.');
+}
+
+else if (expired.toLowerCase() === 'no') {
+
+    if (productsBought < 3  && premiumMember.toLowerCase() === 'no') {
+        alert('Sorry, your offer is invalid.');
+    }
+
+    else {
+        alert('Your offer is valid!');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
