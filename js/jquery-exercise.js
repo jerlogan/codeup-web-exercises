0d2c263b68778a71454287fd8fb6d6f084f03fab
jerlogan <jeremy.logan.web@gmail.com>
2019-02-19 11:20:58 -0600

    jquery-exercise complete

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,7 +1,27 @@
 "use strict";
 
-$(document).ready(function() {
-    alert('The DOM has finished loading.');
-
+$('.header-one').click(function() {
+    $(this).css('background-color', 'green');
 });
+
+$('.paragraph').dblclick(function(){
+    $(this).css('font-size', '18px');
+});
+
+$('.unordered-list li').hover(
+    function () {
+        $(this).css('color', 'red')
+    },
+    function () {
+        $(this).css('color', 'black')
+    }
+);
+
+
+
+
+
+
+
+
 
