585d92d81105d5d755d6ed01b3a3c21c5bc4cad0
jerlogan <jeremy.logan.web@gmail.com>
2019-03-01 17:08:39 -0600

    added all icons to tic tac toe

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -12,34 +12,36 @@ $(document).ready(function(){
     $('.start-game').click(function(e) {
         e.preventDefault();
 
+        $(this).hide();
+        $('.player-one-turn-info').show();
         gameStarted = true;
-
-        if (turnNumber % 2 === 1) {
-            $(this).hide();
-            $('.player-one-turn-info').show();
-
-            console.log(turnNumber);
-        } else if (turnNumber % 2 === 0) {
-            $(this).hide();
-            $('.player-two-turn-info').show();
-            console.log(turnNumber);
-        }
-        turnNumber++;
+        console.log(gameStarted);
 
     });
 
     //conditional checking which turn it is: player 1 (number is odd) or player 2 (number is even)
 
-        $('.small-square').click(function(){
-            if (turnNumber % 2 === 1) {
-                $(this).addClass('player-one-icon')
-            }
-            else if (turnNumber % 2 === 0) {
-                $(this).addClass('player-two-icon');
-            }
-            turnNumber++;
+console.log(gameStarted);
 
+        $('.small-square').click(function () {
+            if (gameStarted) {
+                if (turnNumber % 2 === 1) {
+                    $(this).addClass('player-one-icon');
+                    $('.player-two-turn-info').show();
+                    $('.player-one-turn-info').hide();
+                    console.log(turnNumber);
+
+
+                } else if (turnNumber % 2 === 0) {
+                    $(this).addClass('player-two-icon');
+                    $('.player-one-turn-info').show();
+                    $('.player-two-turn-info').hide();
+                    console.log(turnNumber);
+                }
+                turnNumber++;
+            }
         });
+
 
 
 
